e657450ce1cfe2268de6b09301c5d0f17550c8d3
Randy Kipkurui <randy.kipkurui4@gmail.com>
2024-05-29 16:31:26 +0300

    ✨feat: add links to the footer Social Media icons.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,8 +6,12 @@ const Footer = () => {
     <div className="footer-container">
       <p>2024 Nahla Naturals All rights reserverd</p>
       <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <a href="https://www.instagram.com/nahla_naturals?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
+          <AiFillInstagram />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <AiOutlineTwitter />
+        </a>
       </p>
     </div>
   )
