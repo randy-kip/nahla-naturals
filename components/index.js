b4f9573650b8052363d2c387d600f3a43373598b
Randy Kipkurui <randy.kipkurui4@gmail.com>
2024-06-27 10:16:30 +0300

    ♻️refactor: rename the components to be small case. Also update the imports.

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
-export { default as Footer } from './Footer';
-export { default as Layout } from './Layout';
-export { default as Navbar } from './Navbar';
-export { default as Product } from './Product';
-export { default as HeroBanner } from './HeroBanner';
-export { default as FooterBanner } from './FooterBanner';
-export { default as Cart } from './Cart';
+export { default as Footer } from "./footer";
+export { default as Layout } from "./layout";
+export { default as Navbar } from "./navbar";
+export { default as Product } from "./product";
+export { default as HeroBanner } from "./herobanner";
+export { default as FooterBanner } from "./footerbanner";
+export { default as Cart } from "./cart";
