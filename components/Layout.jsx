1d20c7f8c5b43d87a0f897e9bcf0333ff96cab74
Randy Kipkurui <randy.kipkurui4@gmail.com>
2024-07-04 17:26:15 +0300

    🚚chore: rename components and their imports.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Head from "next/head";
 
-import Navbar from "./navbar";
-import Footer from "./footer";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
