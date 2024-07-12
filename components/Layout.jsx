b1d9f2d1150f52fb8ba1a4593544521545ee9323
Randy Kipkurui <randy.kipkurui4@gmail.com>
2024-07-12 11:35:26 +0300

    ➖build: remove speedinsights dependency. ⬆️build: remove cache and reinstall dependencies. 👷chore: add yarnrc.yml file to include necesary configurations.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Navbar from "./Navbar";
 import Footer from "./Footer";
@@ -16,7 +15,7 @@ const Layout = ({ children }) => {
         <Navbar />
       </header>
       <main className="main-container">
-        {children} <Analytics /> <SpeedInsights />
+        {children} <Analytics />
       </main>
       <footer>
         <Footer />
